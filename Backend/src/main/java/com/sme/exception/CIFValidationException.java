dc261966de5f367622d944c07da082e0ab4a13ea
Thu Thu <ttmt104113@gmail.com>
2025-02-27 10:13:38 +0630

    complete currentacc select

--- Backend/src/main/java/com/sme/exception/CIFValidationException.java
+++ Backend/src/main/java/com/sme/exception/CIFValidationException.java
@@ -1,6 +1,6 @@
 package com.sme.exception;
 
-public class  CIFValidationException extends RuntimeException{
+public class   CIFValidationException extends RuntimeException{
     public CIFValidationException(String message){
         super(message);
     }
--- Backend/src/main/java/com/sme/repository/CurrentAccountRepository.java
+++ Backend/src/main/java/com/sme/repository/CurrentAccountRepository.java
@@ -1,6 +1,8 @@
 package com.sme.repository;
 
 import com.sme.entity.CurrentAccount;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,5 +11,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CurrentAccountRepository extends JpaRepository<CurrentAccount, Long> {
     boolean existsByCifId(Long cifId);
+    Page<CurrentAccount> findAll(Pageable pageable);
+
 
 }
